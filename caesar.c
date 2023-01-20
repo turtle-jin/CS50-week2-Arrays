fe269fe854e5477b96eae8a67d3a013f14fba221
JinJin <codingjinjin@gmail.com>
2023-01-19 23:40:58 -0600

    add rotate function, complete main

--- caesar.c
+++ caesar.c
@@ -20,7 +20,47 @@ int main(int argc, string argv[])
         if (key[i] < 48 || key[i] > 57)
         {
             printf("Usage: ./caesar key\n");
+            return 1;
         }
-        return 1;
+        
     }
+// Convert argv[1] from a `string` to an `int`
+    int k = atoi(key);
+
+    // Prompt user for plaintext
+
+    string p_text = get_string("plaintext:  ");
+    printf("ciphertext: ");
+
+    int len = strlen(p_text);
+
+    for (int j = 0 ; j < len ; j++)
+    {
+        char c = p_text[j];
+        //printf("%c\n", c );
+        char c_text = rotate(c, k);
+        printf("%c", c_text);
+    }
+    printf("\n");
+
+}
+
+char rotate(char character, int k) //function to rotate each char
+{
+    int cipher_letter = 0;
+    if (isalpha(character) && isupper(character))  // if alphabetic , shift plaintext by key, preserving case
+    {
+
+        cipher_letter = (((int)character - 65 + k) % 26) + 65;
+    }
+    else if (isalpha(character) && islower(character))
+    {
+        cipher_letter = (((int)character - 97 + k) % 26) + 97;
+    }
+    else
+    {
+        cipher_letter += (int)character;
+    }
+
+    return (char)cipher_letter;
 }
